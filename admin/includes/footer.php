a8f963e8d3bf27709ab1527f5746f42ff617b9bd
SANTOSH KUMAR SAH <santoshkumar816102@gmail.com>
2024-05-22 08:44:24 +0530

    Project

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+      <div class="clearfix">
+          <div class="copyright pull-left"><b>Online Diagnostic Lab Management System @ 2020</b></div>
+      </div>
+    </footer>
+  </div>
