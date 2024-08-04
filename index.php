c77a1f1e921133df829a90ec4cd2f58ce26f95ba
SANTOSH KUMAR SAH <104618650+santoshkrsah@users.noreply.github.com>
2024-08-04 08:41:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE HTML>
-<html>
+<html> 
 <head>
 <title>Online Diagnostic Lab Management System | Home Page</title>
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
